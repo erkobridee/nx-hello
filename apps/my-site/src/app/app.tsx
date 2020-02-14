65b77eaafb24b89ea32ca001feca89347d2d696e
Erko Bridee <erko.bridee@gmail.com>
2020-02-14 17:14:25 +0100

    feat(my-site): call the api

--- apps/my-site/src/app/app.tsx
+++ apps/my-site/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import './app.scss';
 
@@ -10,6 +10,16 @@ import { Route, Link } from 'react-router-dom';
 import { PageTitle } from '@nx-hello/ui-header';
 
 export const App = () => {
+  const [apiResponse, setApiResponse] = React.useState({
+    message: 'Loading...'
+  });
+
+  React.useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(setApiResponse);
+  }, []);
+
   /*
    * Replace the elements below with your own.
    *
@@ -20,88 +30,10 @@ export const App = () => {
       <header className="flex">
         <Logo width="75" height="75" />
         <PageTitle />
-        <h1>Welcome to my-site!</h1>
       </header>
+
       <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <img src={star} className="material-icons" alt="" />
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
-            >
-              Nx video course
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/getting-started/what-is-nx"
-            >
-              Nx video tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://connect.nrwl.io/">
-              <img
-                height="36"
-                alt="Nrwl Connect"
-                src="https://connect.nrwl.io/assets/img/CONNECT_ColorIcon.png"
-              />
-              <span className="gutter-left">Nrwl Connect</span>
-            </a>
-          </li>
-        </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-`}</pre>
-        </details>
+        <p>{apiResponse.message}</p>
       </main>
 
       {/* START: routes */}
