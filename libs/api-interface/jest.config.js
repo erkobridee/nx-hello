5540d50bfabec4be82e2a4d4421821ea5d04e193
Erko Bridee <erko.bridee@gmail.com>
2020-10-12 11:00:32 +0200

    refactor(project): nx migration to 10.3 second part

--- libs/api-interface/jest.config.js
+++ libs/api-interface/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'api-interface',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/api-interface',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'api-interface',
 };
--- libs/ui-header/jest.config.js
+++ libs/ui-header/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'ui-header',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/ui-header',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'ui-header',
 };
